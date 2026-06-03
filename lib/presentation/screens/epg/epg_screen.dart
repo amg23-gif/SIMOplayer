6742ac442486b2e513ba0b3c6b614919138edd99
amg23-gif <sebsichaf@gmail.com>
2026-06-03 23:18:06 +0100

    fix: explicit dart:ui TextDirection import

--- lib/presentation/screens/epg/epg_screen.dart
+++ lib/presentation/screens/epg/epg_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' show TextDirection;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import '../../../core/constants/app_constants.dart';
