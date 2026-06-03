b138e406407380287e9af166e5702d7305a72619
amg23-gif <sebsichaf@gmail.com>
2026-06-03 22:30:59 +0100

    feat: add lib/core/router/app_router.g.dart

---
+++ lib/core/router/app_router.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// يُولَّد هذا الملف تلقائياً عبر: dart run build_runner build
+// شغّل: flutter pub run build_runner build --delete-conflicting-outputs
+
+part of 'app_router.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appRouterHash() => r'placeholder_hash_run_build_runner';
+
+/// See also [appRouter].
+@ProviderFor(appRouter)
+final appRouterProvider = Provider<GoRouter>.internal(
+  appRouter,
+  name: r'appRouterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appRouterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AppRouterRef = ProviderRef<GoRouter>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
